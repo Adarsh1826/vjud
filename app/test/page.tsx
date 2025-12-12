3e1695eccc5cd9e5f7752f572ad47ba9f4780b50
adarsh singh <adsredmi182615@gmail.com>
2025-12-12 19:56:31 +0530

    started visualizer

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,7 +1,8 @@
 import PageDataFetch from "@/component/auto/urifetch";
-
+import VisualizeFlow from "@/component/visuals/visual";
 export default function Test(){
     return(
-       <PageDataFetch uri={"https://codeforces.com/contest/2175/problem/A"}/>
+    //    <PageDataFetch uri={"https://codeforces.com/contest/2175/problem/A"}/>
+    <VisualizeFlow />
     )
 }
---
+++ component/visuals/visual.tsx
@@ -0,0 +1,7 @@
+export default function VisualizeFlow(){
+    return(
+        <div>
+            Hii there
+        </div>
+    )
+}
